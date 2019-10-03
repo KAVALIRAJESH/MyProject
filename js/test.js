03cd1b802ae29ec638e7bd294c146be962ac0736
rajesh <kavalirajesh123@gmail.com>
2019-10-03 23:18:02 +0530

    first commit

---
+++ js/test.js
@@ -0,0 +1,14 @@
+// document.getElementById('signupbtn').addEventListener("mouseover", function(){
+//     alert("hmm 2");
+// });
+
+// document.getElementById('signupbtn').addEventListener("mouseout", function(){
+//     console.log("hmm 1");
+// });
+//  var myobje = new fuckobj;
+
+
+
+// function myfun(){
+//     alert('hmm')
+// }
